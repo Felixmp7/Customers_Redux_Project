27ce27d5b1fd78ed9fd45302638e362e9ad2b8be
Felix Pacheco <devfex.19@gmail.com>
2020-09-03 02:23:12 -0400

    adding middleware to use promise redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore, compose } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
 import reducers from '../reducers/index';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -7,5 +8,5 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
   reducers,
   {},
-  composeEnhancers()
+  composeEnhancers(applyMiddleware(promiseMiddleware))
 );
