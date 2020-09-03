f825986afee30eac11a5aec6a9ce055a72ff281f
Felix Pacheco <devfex.19@gmail.com>
2020-09-03 01:45:50 -0400

    maping store and calling selector

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types'
 import Button from "@material-ui/core/Button";
 import { connect } from 'react-redux'
 import {_fetchCostumers} from '../../actions/fetchCostumers'
+import { getCustomerDataFromStore } from "../../selectors/customers";
 import { withRouter } from 'react-router-dom';
 import './index.css';
 
@@ -17,6 +18,7 @@ const CustomerPage = ({
   history,
   fetchCostumers,
 }) => {
+  
   useEffect(() => {
     fetchCostumers();
   }, []);
@@ -39,120 +41,7 @@ const CustomerPage = ({
 };
 
 CustomerPage.defaultProps = {
-  customerData: [
-  {
-    id: "30000001",
-    dni: "30000001",
-    name: "Pablo Alborán",
-    age: 30,
-    avatar: 'm',
-  },
-  {
-    id: "500000",
-    dni: "500000",
-    name: "José Enrique",
-    age: 40,
-    avatar: 'm',
-  },
-  {
-    dni: "6868686",
-    name: "Gabriel Pacheco",
-    age: 10,
-    avatar: 'm',
-    id: "XSCfaW5",
-  },
-  {
-    name: "Doris Alonso",
-    dni: "99999",
-    age: 11,
-    avatar: 'w',
-    id: "kxmVQVd",
-  },
-  {
-    id: "30000001",
-    dni: "30000001",
-    name: "Eliana Guerrero",
-    age: 30,
-    avatar: 'w',
-  },
-  {
-    id: "500000",
-    dni: "500000",
-    name: "Arianne Camila",
-    age: 40,
-    avatar: 'w',
-  },
-  {
-    dni: "6868686",
-    name: "Jennifer Marques",
-    age: 10,
-    avatar: 'w',
-    id: "XSCfaW5",
-  },
-  {
-    name: "Jose Daniel Tapiquén",
-    dni: "99999",
-    age: 11,
-    avatar: 'm',
-    id: "kxmVQVd",
-  },
-  {
-    id: "30000001",
-    dni: "30000001",
-    name: "Leonella Berroterán",
-    age: 30,
-    avatar: 'w',
-  },
-  {
-    id: "500000",
-    dni: "500000",
-    name: "Maria Fernanda Gonzalez",
-    age: 40,
-    avatar: 'w',
-  },
-  {
-    dni: "6868686",
-    name: "José Tirado",
-    age: 10,
-    avatar: 'm',
-    id: "XSCfaW5",
-  },
-  {
-    name: "Fibra TV",
-    dni: "99999",
-    age: 11,
-    avatar: 'e',
-    id: "kxmVQVd",
-  },
-  {
-    id: "30000001",
-    dni: "30000001",
-    name: "María Ortiz",
-    age: 30,
-    avatar: 'w',
-  },
-  {
-    id: "500000",
-    dni: "500000",
-    name: "Alejandro Hadyar",
-    age: 40,
-    avatar: 'm',
-  },
-  {
-    dni: "6868686",
-    name: "César Rivas",
-    age: 10,
-    avatar: 'm',
-    id: "XSCfaW5",
-  },
-  {
-    name: "Manuel Carrero",
-    dni: "99999",
-    age: 11,
-    avatar: 'm',
-    id: "kxmVQVd",
-  },
-  ]
+  customerData: []
 }
 
 CustomerPage.propTypes = {
@@ -162,8 +51,12 @@ CustomerPage.propTypes = {
   customerData: PropTypes.array.isRequired,
 };
 
+const mapState = state => ({
+  customerData: getCustomerDataFromStore(state),
+});
+
 const mapDispatch = (dispatch) => ({
   fetchCostumers: () => dispatch(_fetchCostumers()),
 });
 
-export default withRouter(connect(null, mapDispatch)(CustomerPage));
+export default withRouter(connect(mapState, mapDispatch)(CustomerPage));
