95be2c10907ebe9f9ec7e189542c0cb152e6109b
Felix Pacheco <devfex.19@gmail.com>
2020-09-03 20:45:35 -0400

    connecting edit customer with store and getting customer details

--- src/pages/edit-customer/index.js
+++ src/pages/edit-customer/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {ModalContainer} from '../../utils/modal-container/ModalContainer'
+import { connect } from "react-redux";
+import { getDetailsForCostumer } from "../../selectors/customers";
 import './index.css'
 
-const EditCustomerPage = ({ customerName, dni, age }) => {
+
+const EditCustomerPage = ({ dni, customerDetails }) => {
+  const {age, name} = customerDetails
+  // console.log(customerDetails)
   return (
     <div className="containerEditCustomer">
-      <div className="modal">
+      <ModalContainer>
         <h2>Edición del Cliente</h2>
         <div>
           <strong>Nombre:</strong>
-          <i>{customerName}</i>
+          <i>{name}</i>
         </div>
         <div>
           <strong>DNI:</strong>
@@ -19,7 +25,7 @@ const EditCustomerPage = ({ customerName, dni, age }) => {
           <strong>Age:</strong>
           <i>{age}</i>
         </div>
-      </div>
+      </ModalContainer>
     </div>
   );
 };
@@ -27,7 +33,11 @@ const EditCustomerPage = ({ customerName, dni, age }) => {
 EditCustomerPage.propTypes = {
   customerName: PropTypes.string.isRequired,
   dni: PropTypes.string.isRequired,
-  age: PropTypes.string,
+  customerDetails: PropTypes.object.isRequired,
 };
 
-export default EditCustomerPage;
+const mapState = (state, {dni}) => ({
+  customerDetails: getDetailsForCostumer(state,dni)
+})
+
+export default connect(mapState,null)(EditCustomerPage);
