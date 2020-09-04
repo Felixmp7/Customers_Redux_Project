dcc9738e988c21cd76d6a5bc54564083cd2104f3
Felix Pacheco <devfex.19@gmail.com>
2020-09-03 20:44:57 -0400

    renaming api function to fetch customers

--- src/actions/fetchCostumers.js
+++ src/actions/fetchCostumers.js
@@ -1,5 +1,5 @@
 import {FETCH_COSTUMERS} from '../constants/fetchCostumers'
 import { createAction } from 'redux-actions'
-import { apiFetchCustomers } from '../utils/api/api'
+import { apiFetchCustomers } from "../utils/api/customerData";
 
 export const _fetchCostumers = createAction(FETCH_COSTUMERS,apiFetchCustomers);
