597a59fdbb7648ec301f9119de16df36f333afff
Felix Pacheco <devfex.19@gmail.com>
2020-09-03 20:47:56 -0400

    creating new selector with reselect

--- src/selectors/customers.js
+++ src/selectors/customers.js
@@ -1,1 +1,14 @@
+import { createSelector } from "reselect";
+
 export const getCustomerDataFromStore = state => state.customerData;
+
+// export const getDetailsForCostumer = (state, dni) => {
+//   const customerDetails = state.customerData.find( customer => customer.dni === dni);
+//   return customerDetails;
+// };
+
+export const getDetailsForCostumer = 
+  createSelector(
+    (state, dni) => state.customerData.find( customer => customer.dni === dni),
+    customerDetails => customerDetails
+  );
