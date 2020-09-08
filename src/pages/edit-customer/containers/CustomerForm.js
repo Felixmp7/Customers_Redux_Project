a482aefd4bab768f26754bcc91bf32f296a62c03
Felix Pacheco <devfex.19@gmail.com>
2020-09-08 18:25:17 -0400

    practice with validate functions and rendering components

--- src/pages/edit-customer/containers/CustomerForm.js
+++ src/pages/edit-customer/containers/CustomerForm.js
@@ -3,21 +3,42 @@ import React from 'react'
 import { reduxForm, Field } from 'redux-form'; // HOC (objeto con el key form: y el nombre que le daré al form)
 import {setInitialPropsCustomerForm} from '../../../utils/hocs/setInitialPropsCustomerForm';
 
+const isRequired = value => !value && 'Este campo es requerido';
+
+const NameField = ({input, meta}) => (
+  <div>
+    <input {...input} type="text"/>
+    {
+      meta.touched && meta.error && <span>{meta.error}</span>
+    }
+  </div>
+)
+
 const CustomerForm = (props) => {
   console.log(props)
   return (
     <form>
       <div>
         <label htmlFor="name">Customer Name</label>
-        <Field name="name" component="input" type="text" />
+        <Field
+          name="name"
+          validate={isRequired}
+          component={NameField}
+          type="text"
+        />
       </div>
       <div>
         <label htmlFor="dni">Customer DNI</label>
-        <Field name="dni" component="input" type="text" />
+        <Field name="dni" validate={isRequired} component="input" type="text" />
       </div>
       <div>
         <label htmlFor="age">Customer Age</label>
-        <Field name="age" component="input" type="number" />
+        <Field
+          name="age"
+          validate={isRequired}
+          component="input"
+          type="number"
+        />
       </div>
     </form>
   );
