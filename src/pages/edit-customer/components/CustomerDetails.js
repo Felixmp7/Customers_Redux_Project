8e8d838aa2c018655d28c5fbd98dbbe087a223fc
Felix Pacheco <devfex.19@gmail.com>
2020-09-08 14:28:27 -0400

    implementing CustomerDetails

---
+++ src/pages/edit-customer/components/CustomerDetails.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CustomerDetails = ({name,dni,age}) => {
+  return (
+    <>
+      <h2>Edición del Cliente</h2>
+      <div>
+        <strong>Nombre:</strong>
+        <i>{name}</i>
+      </div>
+      <div>
+        <strong>DNI:</strong>
+        <i>{dni}</i>
+      </div>
+      <div>
+        <strong>Age:</strong>
+        <i>{age}</i>
+      </div>
+    </>
+  )
+}
+
+CustomerDetails.propTypes = {
+  name: PropTypes.string.isRequired,
+  dni: PropTypes.string.isRequired,
+  age: PropTypes.string.isRequired,
+};
+
+export default CustomerDetails;
