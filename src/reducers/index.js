249057264f4352c3f6f7b25fed6ee8c90e676449
Felix Pacheco <devfex.19@gmail.com>
2020-09-08 14:29:03 -0400

    adding from reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { customers } from './customers';
+import { reducer as formReducer } from 'redux-form'
 
 export default combineReducers({
-  customerData: customers
+  customerData: customers,
+  form: formReducer
 })
