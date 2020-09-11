b9feda614e2069066daf1e9fb2b61f65e3cbe5df
Felix Pacheco <devfex.19@gmail.com>
2020-09-11 19:55:59 -0400

    moving components

---
+++ src/utils/components/buttons/StyledButton.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  background: transparent;
+  color: #fff;
+  font-size: 1em;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 3px;
+  border: 2px solid #ffffff77;
+  margin-top: 10px;
+  transition: ease 1s;
+  &:hover {
+    transition: ease 1s;
+    // color: #fff;
+    background: #5ab735;
+    border: 2px solid #438925;
+    cursor: pointer;
+  }
+`;
